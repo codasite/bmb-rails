715d5c6e3fbff0fe2f18910465e27877d5fe304c
Barry Molina <bazzaboy@gmail.com>
2023-04-11 07:19:08 -0500

    add boilerplate

---
+++ includes/class-wp-bracket-builder-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/barrymolina
+ * @since      1.0.0
+ *
+ * @package    Wp_Bracket_Builder
+ * @subpackage Wp_Bracket_Builder/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Bracket_Builder
+ * @subpackage Wp_Bracket_Builder/includes
+ * @author     Barry Molina <barry@wstrategies.co>
+ */
+class Wp_Bracket_Builder_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-bracket-builder-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://https://github.com/barrymolina
+ * @since      1.0.0
+ *
+ * @package    Wp_Bracket_Builder
+ * @subpackage Wp_Bracket_Builder/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Bracket_Builder
+ * @subpackage Wp_Bracket_Builder/includes
+ * @author     Barry Molina <barry@wstrategies.co>
+ */
+class Wp_Bracket_Builder_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wp-bracket-builder',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
