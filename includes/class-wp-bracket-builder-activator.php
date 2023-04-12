5104667b41b2c3081ad968005371e1739ae8c482
Barry Molina <bazzaboy@gmail.com>
2023-04-11 22:07:09 -0500

    create sports, teams tables

--- includes/class-wp-bracket-builder-activator.php
+++ includes/class-wp-bracket-builder-activator.php
@@ -30,7 +30,57 @@ class Wp_Bracket_Builder_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $wpdb;
+		$wp_prefix = $wpdb->prefix;
+		$plugin_prefix = 'wpbb_';
+		$prefix = $wp_prefix . $plugin_prefix;
 
+		self::create_sports_table($prefix);
+		// self::create_teams_table($prefix);
+	}
+
+	private static function create_sports_table(string $prefix) {
+		/**
+		 * Create the sports table
+		 */
+
+		global $wpdb;
+		$table_name = $prefix . 'sports';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			name varchar(255) NOT NULL,
+			elimination_type tinyint(1) NOT NULL,
+			number_of_teams mediumint(9) NOT NULL,
+			PRIMARY KEY (id)
+		) $charset_collate;";
+
+		// import dbDelta
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+
+	private static function create_teams_table(string $prefix) {
+		/**
+		 * Create the teams table
+		 */
+
+		global $wpdb;
+		$table_name = $prefix . 'teams';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			name varchar(255) NOT NULL,
+			sport_id mediumint(9) NOT NULL,
+			PRIMARY KEY (id),
+			-- FOREIGN KEY (sport_id) REFERENCES {$prefix}sports(id),
+		) $charset_collate;";
+
+		// import dbDelta
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
 	}
 
 }
