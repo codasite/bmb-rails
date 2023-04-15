b026b660aaca926e88f0f3801349f3a50fb44257
Barry Molina <bazzaboy@gmail.com>
2023-04-15 18:03:45 -0500

    create new tables

--- includes/class-wp-bracket-builder-activator.php
+++ includes/class-wp-bracket-builder-activator.php
@@ -35,12 +35,14 @@ class Wp_Bracket_Builder_Activator {
 		$plugin_prefix = 'bracket_builder_';
 		$prefix = $wp_prefix . $plugin_prefix;
 
-		self::create_sports_table($prefix);
+		// self::create_sports_table($prefix);
 		self::create_teams_table($prefix);
-		self::create_tournament_table($prefix);
-		self::create_rounds_table($prefix);
 		self::create_brackets_table($prefix);
-		// self::create_predictions_table($prefix);
+		self::create_rounds_table($prefix);
+		self::create_nodes_table($prefix);
+		self::create_seeds_table($prefix);
+		self::create_user_brackets_table($prefix);
+		self::create_predictions_table($prefix);
 	}
 
 	private static function create_sports_table(string $prefix) {
@@ -75,9 +77,7 @@ class Wp_Bracket_Builder_Activator {
 		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
 			id mediumint(9) NOT NULL AUTO_INCREMENT,
 			name varchar(255) NOT NULL,
-			sport_id mediumint(9) NOT NULL,
-			PRIMARY KEY (id),
-			FOREIGN KEY (sport_id) REFERENCES {$prefix}sports(id) ON DELETE CASCADE
+			PRIMARY KEY (id)
 		) $charset_collate;";
 
 		// import dbDelta
@@ -85,22 +85,19 @@ class Wp_Bracket_Builder_Activator {
 		dbDelta($sql);
 	}
 
-	private static function create_tournament_table(string $prefix) {
+	private static function create_brackets_table(string $prefix) {
 		/**
 		 * Create the tournaments table
 		 */
 
 		global $wpdb;
-		$table_name = $prefix . 'tournaments';
+		$table_name = $prefix . 'brackets';
 		$charset_collate = $wpdb->get_charset_collate();
 
 		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
 			id mediumint(9) NOT NULL AUTO_INCREMENT,
 			name varchar(255) NOT NULL,
-			sport_id mediumint(9) NOT NULL,
-			wildcard_teams mediumint(9) NOT NULL,
-			PRIMARY KEY (id),
-			FOREIGN KEY (sport_id) REFERENCES {$prefix}sports(id) ON DELETE CASCADE
+			PRIMARY KEY (id)
 		) $charset_collate;";
 
 		// import dbDelta
@@ -120,9 +117,9 @@ class Wp_Bracket_Builder_Activator {
 		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
 			id mediumint(9) NOT NULL AUTO_INCREMENT,
 			name varchar(255) NOT NULL,
-			tournament_id mediumint(9) NOT NULL,
+			bracket_id mediumint(9) NOT NULL,
 			PRIMARY KEY (id),
-			FOREIGN KEY (tournament_id) REFERENCES {$prefix}tournaments(id)
+			FOREIGN KEY (bracket_id) REFERENCES {$prefix}brackets(id)
 		) $charset_collate;";
 
 		// import dbDelta
@@ -130,22 +127,71 @@ class Wp_Bracket_Builder_Activator {
 		dbDelta($sql);
 	}
 
-	private static function create_brackets_table(string $prefix) {
+	private static function create_nodes_table(string $prefix) {
+		/**
+		 * Create the nodes table
+		 */
+
+		global $wpdb;
+		$table_name = $prefix . 'nodes';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			round_id mediumint(9) NOT NULL,
+			lft int(11) NOT NULL,
+			rgt int(11) NOT NULL,
+			in_order int(11) NOT NULL,
+			PRIMARY KEY (id),
+			FOREIGN KEY (round_id) REFERENCES {$prefix}rounds(id)
+		) $charset_collate;";
+
+		// import dbDelta
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+
+	private static function create_seeds_table(string $prefix) {
+		/**
+		 * Create the seeds table
+		 */
+
+		global $wpdb;
+		$table_name = $prefix . 'seeds';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			bracket_id mediumint(9) NOT NULL,
+			team_id mediumint(9) NOT NULL,
+			node_id mediumint(9) NOT NULL,
+			PRIMARY KEY (id),
+			FOREIGN KEY (bracket_id) REFERENCES {$prefix}brackets(id),
+			FOREIGN KEY (team_id) REFERENCES {$prefix}teams(id),
+			FOREIGN KEY (node_id) REFERENCES {$prefix}nodes(id)
+		) $charset_collate;";
+
+		// import dbDelta
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+
+	private static function create_user_brackets_table(string $prefix) {
 		/**
 		 * Create the brackets table
 		 */
 
 		global $wpdb;
-		$table_name = $prefix . 'brackets';
+		$table_name = $prefix . 'user_brackets';
 		$charset_collate = $wpdb->get_charset_collate();
 
 		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
 			id mediumint(9) NOT NULL AUTO_INCREMENT,
 			name varchar(255) NOT NULL,
 			cust_id mediumint(9),
-			tournament_id mediumint(9) NOT NULL,
+			bracket_id mediumint(9) NOT NULL,
 			PRIMARY KEY (id),
-			FOREIGN KEY (tournament_id) REFERENCES {$prefix}tournaments(id)
+			FOREIGN KEY (bracket_id) REFERENCES {$prefix}brackets(id)
 		) $charset_collate;";
 
 		// import dbDelta
@@ -153,7 +199,7 @@ class Wp_Bracket_Builder_Activator {
 		dbDelta($sql);
 	}
 
-	private static function create_preditions_table(string $prefix) {
+	private static function create_predictions_table(string $prefix) {
 		/**
 		 * Create the predictions table
 		 */
@@ -164,16 +210,13 @@ class Wp_Bracket_Builder_Activator {
 
 		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
 			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			user_bracket_id mediumint(9) NOT NULL,
+			node_id mediumint(9) NOT NULL,
 			team_id mediumint(9) NOT NULL,
-			bracket_id mediumint(9) NOT NULL,
-			round_id mediumint(9) NOT NULL,
-			lft int(11) NOT NULL,
-			rgt int(11) NOT NULL,
-			in_order int(11) NOT NULL,
 			PRIMARY KEY (id),
-			FOREIGN KEY (team_id) REFERENCES {$prefix}teams(id),
-			FOREIGN KEY (bracket_id) REFERENCES {$prefix}brackets(id),
-			FOREIGN KEY (round_id) REFERENCES {$prefix}rounds(id)
+			FOREIGN KEY (user_bracket_id) REFERENCES {$prefix}user_brackets(id),
+			FOREIGN KEY (node_id) REFERENCES {$prefix}nodes(id),
+			FOREIGN KEY (team_id) REFERENCES {$prefix}teams(id)
 		) $charset_collate;";
 
 		// import dbDelta
@@ -181,4 +224,3 @@ class Wp_Bracket_Builder_Activator {
 		dbDelta($sql);
 	}
 }
-
