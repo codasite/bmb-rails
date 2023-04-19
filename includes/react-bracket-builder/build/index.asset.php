101655f9958b13ac9d5fb774fac690cbc32eb6de
Barry Molina <bazzaboy@gmail.com>
2023-04-19 15:09:21 -0500

    round input padding

--- includes/react-bracket-builder/build/index.asset.php
+++ includes/react-bracket-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '316f5f2fea407524ad69');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ca1a9ba5400caf19ec79');
