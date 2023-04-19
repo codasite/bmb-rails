6294ea11b96d7c7563786dc1bfd2f8856a8f4387
Barry Molina <bazzaboy@gmail.com>
2023-04-18 21:48:51 -0500

    round input fields

--- includes/react-bracket-builder/build/index.asset.php
+++ includes/react-bracket-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ec6a6e7023556b682d9e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '6af4e4ed3b882b9a6f36');
