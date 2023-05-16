9c39f825af0a1e737dfc06ad3a742d4737f628cd
Mark <markmaceachen@live.com>
2023-05-15 20:16:57 -0600

    logo overlay using javascript

---
+++ includes/react-bracket-builder/src/preview/components/Preview.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// create function component called Preview that takes in a prop called bracket
+const Preview = ({ bracket }) => {
+    // return a div with a class of preview
+    return <div className="preview">Preview</div>;
+    };
+
+// export the Preview component
+export default Preview;
