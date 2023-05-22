331bb6b74ab239a606567ba7229822e4a7e2b114
Barry Molina <bazzaboy@gmail.com>
2023-05-22 13:14:01 -0500

    lambda service

--- includes/service/class-wp-bracket-builder-aws-service.php
+++ includes/service/class-wp-bracket-builder-aws-service.php
@@ -9,7 +9,7 @@ use Aws\Lambda\LambdaClient;
 #snippet-start:[php.example_code.lambda.service]
 
 
-class LambdaServicex {
+class LambdaService {
 	protected LambdaClient $lambdaClient;
 
 	public function __construct(
@@ -28,6 +28,45 @@ class LambdaServicex {
 			'version' => $version,
 			'profile' => $profile,
 		]);
+	}
+
+	public function html_to_image($html) {
+		// $functionName = 'html-to-image';
+		// $params = [
+		// 	'html' => $html,
+		// ];
+		// $result = $this->invoke($functionName, $params);
+		// return $result['Payload']->getContents();
+		$res = $this->image_from_api($html);
+		return $res;
+	}
+
+	public function image_from_api($html) {
+		$convert_url = 'http://localhost:8080/convert';
+		// Make a request to the convert url using POST, content type application/json, and the html as the body, and accept *
+		$body = array(
+			'html' => $html,
+		);
+		// convert body to json
+
+
+		$res = wp_remote_post($convert_url, array(
+			'method' => 'POST', // 'GET' or 'POST
+			'timeout' => 45,
+			'headers' => array(
+				'Content-Type' => 'application/json',
+				'Accept' => '*',
+			),
+			'body' => json_encode($body)
+		));
+
+		if (is_wp_error($res) || wp_remote_retrieve_response_code($res) !== 200) {
+			return new WP_Error('error', __('There was an error converting the html to an image', 'text-domain'), array('status' => 500));
+		}
+
+		// get the response body as json
+		$res_body = json_decode(wp_remote_retrieve_body($res));
+		return $res_body;
 	}
 
 	public function invoke($functionName, $params, $logType = 'None') {
