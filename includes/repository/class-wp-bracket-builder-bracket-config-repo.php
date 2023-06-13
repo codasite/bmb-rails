87c6e1c1146138e5c27acf27629b7c3d7d180e86
barrymolina <bazzaboy@gmail.com>
2023-06-13 16:59:37 +0000

    store configs under single key, update cart validation

--- includes/repository/class-wp-bracket-builder-bracket-config-repo.php
+++ includes/repository/class-wp-bracket-builder-bracket-config-repo.php
@@ -2,14 +2,31 @@
 require_once plugin_dir_path(dirname(__FILE__)) . 'domain/class-wp-bracket-builder-bracket-config.php';
 require_once plugin_dir_path(dirname(__FILE__)) . 'class-wp-bracket-builder-utils.php';
 
+// a constant for the session key
+define('WPBB_BRACKET_CONFIG_SESSION_KEY', 'wpbb_bracket_config');
 
 class Wp_Bracket_Builder_Bracket_Config_Repository {
 
 	public function add(Wp_Bracket_Builder_Bracket_Config $config, string $theme_mode = ''): Wp_Bracket_Builder_Bracket_Config {
 		$utils = new Wp_Bracket_Builder_Utils();
-		$session_key = $this->get_session_key($theme_mode);
-		$utils->set_session_value($session_key, $config);
-		return $config;
+		// get the current configs
+		$configs = $this->get_all();
+		// add the new config
+		$configs[$theme_mode] = $config;
+		// set the session value
+		$session_key = WPBB_BRACKET_CONFIG_SESSION_KEY;
+		$utils->set_session_value($session_key, $configs);
+		return $configs[$theme_mode];
+	}
+
+	public function get_all(): array {
+		$utils = new Wp_Bracket_Builder_Utils();
+		$session_key = WPBB_BRACKET_CONFIG_SESSION_KEY;
+		$configs = $utils->get_session_value($session_key);
+		if ($configs) {
+			return $configs;
+		}
+		return [];
 	}
 
 	public function get(string $theme_mode = ''): Wp_Bracket_Builder_Bracket_Config | null {
@@ -17,15 +34,10 @@ class Wp_Bracket_Builder_Bracket_Config_Repository {
 			return null;
 		}
 		$utils = new Wp_Bracket_Builder_Utils();
-		$session_key = $this->get_session_key($theme_mode);
-		$config = $utils->get_session_value($session_key);
-		if ($config) {
-			return $config;
+		$configs = $this->get_all();
+		if (isset($configs[$theme_mode])) {
+			return $configs[$theme_mode];
 		}
 		return null;
-	}
-
-	private function get_session_key(string $theme_mode = ''): string {
-		return $theme_mode ? 'wpbb_bracket_config_' . $theme_mode : 'wpbb_bracket_config';
 	}
 }
