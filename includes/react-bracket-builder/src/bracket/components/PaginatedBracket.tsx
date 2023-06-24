676a163b31070e9d77ff0a3ed37e279894adc97c
Barry Molina <bazzaboy@gmail.com>
2023-06-24 04:39:34 -0500

    render responsive bracket

--- includes/react-bracket-builder/src/bracket/components/PaginatedBracket.tsx
+++ includes/react-bracket-builder/src/bracket/components/PaginatedBracket.tsx
@@ -6,3 +6,31 @@ import { MatchTree, Round, MatchNode, Team } from '../models/MatchTree';
 import LineTo, { SteppedLineTo, Line } from 'react-lineto';
 //@ts-ignore
 import { ReactComponent as BracketLogo } from '../../assets/BMB-ICON-CURRENT.svg';
+
+interface PairedBracketProps {
+	matchTree: MatchTree;
+	bracketName?: string;
+	canEdit?: boolean;
+	canPick?: boolean;
+	darkMode?: boolean;
+	setMatchTree?: (matchTree: MatchTree) => void;
+}
+
+export const PaginatedBracket = (props: PairedBracketProps) => {
+	const {
+		matchTree,
+		bracketName,
+		canEdit,
+		canPick,
+		darkMode,
+		setMatchTree,
+	} = props;
+
+	return (
+		<div className='wpbb-paginated-bracket-container'>
+			<div className='wpbb-paginated-bracket'>
+				hiiiii
+			</div>
+		</div>
+	)
+}
