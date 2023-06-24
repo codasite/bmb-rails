f21f3148d681511452622480006e85ccb6c909bf
Barry Molina <bazzaboy@gmail.com>
2023-06-24 08:28:51 -0500

    load from rounds works

--- includes/react-bracket-builder/src/features/match_tree/matchTreeSlice.ts
+++ includes/react-bracket-builder/src/features/match_tree/matchTreeSlice.ts
@@ -1,28 +1,32 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { MatchTree } from "../../bracket/models/MatchTree";
+import { RoundRes } from "../../api/types/bracket";
 
 interface MatchTreeState {
-	matchTree: MatchTree | null
+	// matchTree: MatchTree | null
+	rounds: RoundRes[] | null
 }
 
 const initialState: MatchTreeState = {
-	matchTree: null
+	rounds: null
 }
 
 export const matchTreeSlice = createSlice({
 	name: 'matchTree',
 	initialState,
 	reducers: {
-		setMatchTree: (state, action: PayloadAction<MatchTree>) => {
-			state.matchTree = action.payload;
+		setMatchTree: (state, action: PayloadAction<RoundRes[]>) => {
+			state.rounds = action.payload;
 		}
 	}
 });
 
 export const { setMatchTree } = matchTreeSlice.actions;
 
-export const selectMatchTree = (state: RootState) => state.matchTree.matchTree;
+// export const selectMatchTree = (state: RootState) => state.matchTree.matchTree;
+// export const selectMatchTree = (state: RootState) => state.matchTree.rounds;
+export const selectMatchTree = (state: RootState) => state.matchTree.rounds ? MatchTree.fromRounds(state.matchTree.rounds) : null;
 
 export default matchTreeSlice.reducer;
 
