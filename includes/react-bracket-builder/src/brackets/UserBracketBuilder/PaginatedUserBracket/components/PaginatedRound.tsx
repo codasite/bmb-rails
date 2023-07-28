387a7599ff22e64ed2d532197fbf3e63479dd74d
Barry Molina <bazzaboy@gmail.com>
2023-07-28 17:16:21 -0500

    correcly display the first column on a paginated round page

--- includes/react-bracket-builder/src/brackets/UserBracketBuilder/PaginatedUserBracket/components/PaginatedRound.tsx
+++ includes/react-bracket-builder/src/brackets/UserBracketBuilder/PaginatedUserBracket/components/PaginatedRound.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { ActionButton } from '../../../shared/components/ActionButton'
 import { MatchTree, Round, MatchNode, Team } from '../../../shared/models/MatchTree';
 import { MatchColumn } from '../../../shared/components/MatchColumn'
-import { Direction } from '../../../shared/constants'
+import { Direction, bracketConstants } from '../../../shared/constants'
 import { Nullable } from '../../../../utils/types';
 import { useAppSelector, useAppDispatch } from '../../../shared/app/hooks'
 import { nextPage, selectCurrentPage, selectNumPages } from '../../../shared/features/bracketNavSlice';
 import { selectMatchTree } from '../../../shared/features/matchTreeSlice';
-import { getTargetHeight, getTeamClassName, getMatchHeight } from '../../../shared/utils';
+import {
+	getTargetHeight,
+	getTeamClassName,
+	getMatchHeight,
+	getFirstRoundMatchHeight,
+	getTargetMatchHeight,
+} from '../../../shared/utils';
+
+const {
+	teamHeight,
+} = bracketConstants
 
 const PaginatedRoundHeader = (props) => {
 	const {
@@ -50,18 +60,21 @@ export const PaginatedRound = (props) => {
 	if (!matchTree) {
 		return null
 	}
+	console.log('rounds', matchTree.rounds)
 	const numRounds = matchTree.rounds.length
 	const numDirections = 2 // 1 direction for single trees, 2 for double trees
 	const directions = [Direction.TopLeft, Direction.TopRight]
 	const roundPageMaxIndex = numRounds * numDirections - 1 // This is the index of the last page of the last round
 	const roundPageOffset = 1 // Number of pages that have been added before the first round
 
-	const roundPage = currentPage - roundPageOffset // this should not be less than 0
-	// const num
+	const roundPage = currentPage - roundPageOffset // The current page ignoring pages before the first round
 	const roundIndex = Math.floor((roundPageMaxIndex - roundPage) / numDirections)
-	const direction = directions[(roundPageMaxIndex - roundPage) % numDirections]
+	const direction = directions[roundPage % numDirections]
+
+	// const direction = directions[(roundPageMaxIndex - roundPage) % numDirections]
 	const round = matchTree.rounds[roundIndex]
 	let matches: Nullable<MatchNode>[] = []
+	console.log('direction', direction)
 	if (direction === Direction.TopLeft) {
 		// if going from left to right, only show the first half of the matches
 		matches = round.matches.slice(0, round.matches.length / 2)
@@ -70,7 +83,18 @@ export const PaginatedRound = (props) => {
 		matches = round.matches.slice(round.matches.length / 2, round.matches.length)
 	}
 	const targetHeight = getTargetHeight(numRounds)
+	const firstRoundMatchHeight = getFirstRoundMatchHeight(targetHeight, numDirections, numRounds, teamHeight)
+	const roundReverseIndex = numRounds - roundIndex - 1
+	const totalMatchHeight = getTargetMatchHeight(firstRoundMatchHeight, roundReverseIndex)
+	const matchHeight = getMatchHeight(round.depth)
+	const matchSpacing = totalMatchHeight - matchHeight
+	console.log('round', round)
+	console.log('targetHeight', targetHeight)
 	console.log('roundIndex', roundIndex)
+	console.log('firstRoundMatchHeight', firstRoundMatchHeight)
+	console.log('totalMatchHeight', totalMatchHeight)
+	console.log('matchHeight', matchHeight)
+	console.log('matchSpacing', matchSpacing)
 	// const round = matchTree.rounds[numRounds - currentPage]
 
 	// console.log('PaginatedRound', props)
@@ -85,7 +109,8 @@ export const PaginatedRound = (props) => {
 					round={round}
 					matches={matches}
 					direction={direction}
-					targetHeight={targetHeight}
+					matchBoxHeight={matchHeight}
+					matchBoxSpacing={matchSpacing}
 				/>
 				{/* <MatchColumn /> */}
 			</div>
