ba5e1c6992cf621ab22afe63042c0ecf62bce035
Barry Molina <bazzaboy@gmail.com>
2023-08-23 12:56:54 -0500

    tailwind my profile

--- public/js/wp-bracket-builder-public.js
+++ public/js/wp-bracket-builder-public.js
@@ -33,20 +33,50 @@
 
 console.log('hello from script')
 tailwind.config = {
-	prefix: 'bb-',
+	prefix: 'tw-',
 	corePlugins: {
 		preflight: false,
 	},
 	theme: {
+		extend: {
+			opacity: {
+				'15': '0.15',
+			},
+		},
 		spacing: {
 			'4': '4px',
 			'8': '8px',
 			'10': '10px',
 			'12': '12px',
+			'15': '15px',
 			'16': '16px',
 			'24': '24px',
 			'30': '30px',
 			'60': '60px',
+		},
+		colors: {
+			'black': '#000',
+			'white': '#fff',
+			'green': '#05FF3C',
+			'blue': '#2137ff',
+		},
+		borderRadius: {
+			'4': '4px',
+			'8': '8px',
+			'16': '16px',
+		},
+		fontSize: {
+			'12': '12px',
+			'16': '16px',
+			'20': '20px',
+			'24': '24px',
+			'30': '30px',
+			'48': '48px',
+		},
+		fontWeight: {
+			'500': '500',
+			'600': '600',
+			'700': '700',
 		}
 	}
 }
