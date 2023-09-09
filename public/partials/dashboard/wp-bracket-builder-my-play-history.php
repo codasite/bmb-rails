3afd3a0414eb3404598eccebd1e07ff6d5598230
Mark <markmaceachen@live.com>
2023-09-08 20:27:50 -0600

    before merge

--- public/partials/dashboard/wp-bracket-builder-my-play-history.php
+++ public/partials/dashboard/wp-bracket-builder-my-play-history.php
@@ -3,51 +3,39 @@ require_once 'wp-bracket-builder-dashboard-common.php';
 $shared_dir = plugin_dir_path(dirname(__FILE__)) . 'shared/';
 require_once $shared_dir . 'wp-bracket-builder-partials-common.php';
 require_once $shared_dir . 'wp-bracket-builder-tournaments-common.php';
+require_once plugin_dir_path(dirname(__FILE__, 3)) . 'includes/repository/class-wp-bracket-builder-bracket-play-repo.php';
 
-$plays = array(
-	array(
-		"tournament_name" => "NCAA College Football 2024 hosted by Ahmad Merritt",
-		"user_rank" => 67203,
-		"user_score" => .30,
-		"play_id" => 1,
-		"tournament_id" => 1,
-		"trend_up" => true,
-		"complete" => false,
-	),
-	array(
-		"tournament_name" => "NCAA College Football 2024",
-		"user_rank" => 1205,
-		"user_score" => .75,
-		"play_id" => 2,
-		"tournament_id" => 2,
-		"trend_up" => false,
-		"complete" => true,
-	),
-	array(
-		"tournament_name" => "NCAA Womens World Series 2024",
-		"user_rank" => 6432,
-		"user_score" => .45,
-		"play_id" => 3,
-		"tournament_id" => 3,
-		"trend_up" => true,
-		"complete" => true,
-	),
-);
+$play_repo = new Wp_Bracket_Builder_Bracket_Play_Repository();
+
+$plays = $play_repo->get_all_by_author(get_current_user_id());
 
 function play_list_item($play) {
-	$tournament_name = $play['tournament_name'];
-	$user_rank = number_format($play['user_rank']);
-	$user_score = $play['user_score'];
-	$complete = $play['complete'];
-	$play_id = $play['play_id'];
-	$tournament_id = $play['tournament_id'];
+	$tournament_name = $play->title;
+	$user_rank = 99999;
+	$complete = $play->status === 'complete';
+	$play_id = $play->id;
+	$tournament_id = $play->tournament_id;
 	$play_link = get_permalink() . 'tournaments/' . $tournament_id . '/play/' . $play_id;
 	$leaderboard_link = get_permalink() . 'tournaments/' . $tournament_id . '/leaderboard';
-	$trend_up = $play['trend_up'];
+	$trend_up = true;
 	$trend_icon = $trend_up ? 'arrow_up.svg' : 'arrow_down.svg';
 	$leaderboard_variant = $complete ? 'final' : 'primary';
-	$user_score_percent = $user_score * 100;
+	$user_score_percent = 75;// $user_score * 100;
 	ob_start();
+
+	// $tournament_name = $play['tournament_name'];
+	// $user_rank = number_format($play['user_rank']);
+	// $user_score = $play['user_score'];
+	// $complete = $play['complete'];
+	// $play_id = $play['play_id'];
+	// $tournament_id = $play['tournament_id'];
+	// $play_link = get_permalink() . 'tournaments/' . $tournament_id . '/play/' . $play_id;
+	// $leaderboard_link = get_permalink() . 'tournaments/' . $tournament_id . '/leaderboard';
+	// $trend_up = $play['trend_up'];
+	// $trend_icon = $trend_up ? 'arrow_up.svg' : 'arrow_down.svg';
+	// $leaderboard_variant = $complete ? 'final' : 'primary';
+	// $user_score_percent = $user_score * 100;
+	// ob_start();
 ?>
 	<div class="tw-flex tw-justify-between tw-p-30 tw-rounded-16 tw-border-2 tw-border-solid tw-border-blue/20 tw-bg-blue/5">
 		<div class="tw-flex tw-flex-col tw-gap-20">
