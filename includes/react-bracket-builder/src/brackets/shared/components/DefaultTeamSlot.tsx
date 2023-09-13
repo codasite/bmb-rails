14e18971d8220bed57b720cbf18f40d8017ac523
Barry Molina <bazzaboy@gmail.com>
2023-09-13 07:30:34 -0500

    active teamslot styels

--- includes/react-bracket-builder/src/brackets/shared/components/DefaultTeamSlot.tsx
+++ includes/react-bracket-builder/src/brackets/shared/components/DefaultTeamSlot.tsx
@@ -27,8 +27,40 @@ export const DefaultTeamSlot = (props: TeamSlotProps) => {
 	} = props
 	// console.log('winner', winner)
 	const teamClass = getTeamClass(match.roundIndex, match.matchIndex, teamPosition)
+	const active = true
+
+	const baseStyles = [
+		teamClass,
+		'tw-flex',
+		'tw-justify-center',
+		'tw-items-center',
+		'tw-whitespace-nowrap',
+		`tw-w-[${width}px]`,
+		`tw-h-[${height}px]`,
+		'tw-text-14',
+		'tw-font-500',
+	]
+
+	const activeStyles = [
+		'tw-bg-dd-blue',
+		'dark:tw-bg-white',
+		'tw-text-white',
+		'dark:tw-text-dd-blue',
+	]
+
+	const inactiveStyles = [
+		'tw-border-2',
+		'tw-border-solid',
+		'tw-border-dd-blue/50',
+		'dark:tw-border-white/50',
+		'tw-text-dd-blue',
+		'dark:tw-text-white',
+	]
+
+	const styles = [...baseStyles, ...(active ? activeStyles : inactiveStyles)].join(' ')
+
 	return (
-		<div className={`${teamClass} tw-flex tw-justify-center tw-items-center tw-whitespace-nowrap tw-w-[${width}px] tw-h-[${height}px] tw-border-2 tw-border-solid tw-border-dd-blue/50 dark:tw-border-white/50 tw-text-14 tw-font-500 tw-text-dd-blue dark:tw-text-white`}>
+		<div className={styles}>
 			<span className={`tw-font-${fontWeight} tw-text-${fontSize}`}>{team ? team.name : ''}</span>
 		</div>
 	)
