d8ae799676505a2dd32021fd547d1b539769311c
Mark <markmaceachen@live.com>
2023-09-13 15:05:32 -0600

    updated links to tournament page from various origins

--- public/partials/wp-bracket-builder-bracket-tournament.php
+++ public/partials/wp-bracket-builder-bracket-tournament.php
@@ -17,15 +17,4 @@ switch ($view) {
         echo 'Error: view must be set to either "leaderboard", "play", or score.';
         break;
 }
-// if ($view === 'leaderboard') {
-//     // render the leaderboard template
-//     include 'wp-bracket-builder-tourney-leaderboard.php';
-// } else if ($view === 'play') {
-//     // render the play template
-//     // This is not working
-//     echo '<div id="wpbb-bracket-builder"></div>';
-// } else {
-//     // An error occurred
-//     echo 'view must be set to either "leaderboard" or "play"';
-// }
 ?>
