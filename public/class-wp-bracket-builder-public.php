2f531ff90920418fe7dc3ad2a21e2c65e34b8121
Barry Molina <bazzaboy@gmail.com>
2023-09-16 02:50:20 -0500

    paginate play history

--- public/class-wp-bracket-builder-public.php
+++ public/class-wp-bracket-builder-public.php
@@ -387,6 +387,7 @@ class Wp_Bracket_Builder_Public {
 		add_rewrite_rule('^dashboard/templates/?', 'index.php?pagename=dashboard&tab=templates', 'top');
 		add_rewrite_rule('^dashboard/tournaments/page/([0-9]+)/?', 'index.php?pagename=dashboard&tab=tournaments&paged=$matches[1]', 'top');
 		add_rewrite_rule('^dashboard/tournaments/?', 'index.php?pagename=dashboard&tab=tournaments', 'top');
+		add_rewrite_rule('^dashboard/play-history/page/([0-9]+)/?', 'index.php?pagename=dashboard&tab=play-history&paged=$matches[1]', 'top');
 		add_rewrite_rule('^dashboard/play-history/?', 'index.php?pagename=dashboard&tab=play-history', 'top');
 		add_rewrite_rule('^bracket_tournament/([^/]+)/([^/]+)', 'index.php?bracket_tournament=$matches[1]&view=$matches[2]', 'top');
 	}
