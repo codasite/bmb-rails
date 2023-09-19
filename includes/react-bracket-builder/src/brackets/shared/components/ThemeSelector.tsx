1e6e3e59738ad56363465e68a5fad0abd55f1868
Barry Molina <bazzaboy@gmail.com>
2023-09-19 17:02:26 -0500

    play tournaments builder updates

--- includes/react-bracket-builder/src/brackets/shared/components/ThemeSelector.tsx
+++ includes/react-bracket-builder/src/brackets/shared/components/ThemeSelector.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 interface ThemeSelectorProps {
-	darkMode: boolean;
-	setDarkMode: (darkMode: boolean) => void;
+	darkMode?: boolean;
+	setDarkMode?: (darkMode: boolean) => void;
 }
 
 export const ThemeSelector = (props: ThemeSelectorProps) => {
@@ -13,7 +13,7 @@ export const ThemeSelector = (props: ThemeSelectorProps) => {
 	return (
 		<div className='tw-flex tw-items-center tw-font-600 tw-gap-14'>
 			<span className='tw-text-dd-blue dark:tw-text-white'>Theme</span>
-			<button onClick={() => setDarkMode(!darkMode)} className='tw-flex tw-items-center tw-justify-end dark:tw-justify-start tw-w-[71px] tw-h-30 tw-px-2 tw-rounded-16 dark:tw-border-2 tw-border-solid tw-border-white tw-cursor-pointer tw-bg-dd-blue dark:tw-bg-none'>
+			<button onClick={() => setDarkMode?.(!darkMode)} className='tw-flex tw-items-center tw-justify-end dark:tw-justify-start tw-w-[71px] tw-h-30 tw-px-2 tw-rounded-16 dark:tw-border-2 tw-border-solid tw-border-white tw-cursor-pointer tw-bg-dd-blue dark:tw-bg-none'>
 				<div className='tw-w-[47px] tw-h-[22px] tw-rounded-16 tw-bg-white tw-text-10 tw-flex tw-items-center tw-justify-center'>
 					<span className='tw-text-dd-blue tw-font-600 tw-text-sans tw-uppercase'>{darkMode ? 'dark' : 'light'}</span>
 				</div>
