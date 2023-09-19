237acb98069a8a84ed9c77c327a7c101870e0839
Barry Molina <bazzaboy@gmail.com>
2023-09-19 07:41:40 -0500

    view play shortcode

---
+++ public/partials/wp-bracket-builder-bracket-play-page.php
@@ -0,0 +1,10 @@
+<?php
+$view = get_query_var('view');
+switch ($view) {
+    case 'view':
+        echo '<div id="wpbb-view-play"></div>';
+        break;
+    default:
+        echo '<div id="wpbb-view-play"></div>';
+        break;
+}
