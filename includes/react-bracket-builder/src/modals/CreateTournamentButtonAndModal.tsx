e37e81ba0d93e91b41edb30bc694045b576d579a
Karl Molina <karlgmolina@gmail.com>
2023-09-20 12:16:00 -0600

    Update opacity and add pointer

--- includes/react-bracket-builder/src/modals/CreateTournamentButtonAndModal.tsx
+++ includes/react-bracket-builder/src/modals/CreateTournamentButtonAndModal.tsx
@@ -10,7 +10,7 @@ export const CreateTournamentButtonAndModal = (props: {
   const [show, setShow] = useState(false);
   return <>
     <button
-      className="tw-border-solid tw-border tw-border-white tw-bg-white/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 tw-p-16 hover:tw-bg-white hover:tw-text-black tw-font-sans tw-text-white tw-uppercase tw-w-full"
+      className="tw-border-solid tw-border tw-border-white tw-bg-white/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 tw-p-16 hover:tw-bg-white hover:tw-text-black tw-font-sans tw-text-white tw-uppercase tw-w-full tw-cursor-pointer"
       onClick={() => setShow(true)}>
       <SignalIcon/>
       <span className="tw-font-700 tw-text-24 ">Create Tournament</span>
@@ -22,16 +22,16 @@ export const CreateTournamentButtonAndModal = (props: {
         <h1 className="tw-text-32 tw-leading-10 tw-text-center tw-font-white tw-whitespace-pre-line tw-mb-50">{`Host a tournament.
  invite & compete with friends.`}</h1>
         <a href={props.myTemplatesUrl}
-          className="tw-border-solid tw-border tw-border-green tw-bg-green/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 md:tw-p-40 hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-20 tw-font-500 tw-mb-15 tw-p-20">
+          className="tw-border-solid tw-border tw-border-green tw-bg-green/20 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 md:tw-p-40 hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-20 tw-font-500 tw-mb-15 tw-p-20">
           <FileIcon/><span>Use a template</span>
         </a>
         <a href={props.bracketTemplateBuilderUrl}
-          className="tw-border-solid tw-border tw-border-white tw-bg-white/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 md:tw-p-40 hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-20 tw-font-500 tw-mb-15 tw-p-20">
+          className="tw-border-solid tw-border tw-border-white tw-bg-white/20 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 md:tw-p-40 hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-20 tw-font-500 tw-mb-15 tw-p-20">
           <PlusIcon/><span>Start from scratch</span>
         </a>
         <button
           onClick={() => setShow(false)}
-          className="tw-bg-white/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 tw-p-12 tw-border-none hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-16 tw-font-500">
+          className="tw-bg-white/15 tw-flex tw-gap-16 tw-items-center tw-justify-center tw-rounded-8 tw-p-12 tw-border-none hover:tw-text-white/75 tw-font-sans tw-text-white tw-uppercase tw-w-full tw-text-16 tw-font-500 tw-cursor-pointer">
           Cancel
         </button>
       </div>
