30120dcbf320329a357d86f9301e06b820545437
Barry Molina <bazzaboy@gmail.com>
2023-09-21 21:58:13 -0500

    sql statement to update tournament play scores

--- includes/service/class-wp-bracket-builder-score-service.php
+++ includes/service/class-wp-bracket-builder-score-service.php
@@ -38,7 +38,8 @@ class Wp_Bracket_Builder_Score_Service {
 	}
 
 	public function score_tournament_plays(Wp_Bracket_Builder_Bracket_Tournament|int|null $tournament) {
-		$point_values = [1, 2, 4, 8, 16, 32];
+		// $point_values = [1, 2, 4, 8, 16, 32];
+		$point_values = [1, 1, 1, 1, 1, 1];
 		echo "Scoring tournament plays";
 		$tournament_data = $this->tournament_repo->get_tournament_data($tournament->id);
 		$tournament_id = $tournament_data['id'];
@@ -46,11 +47,57 @@ class Wp_Bracket_Builder_Score_Service {
 		if (!$tournament_id) {
 			return;
 		}
-		echo "Tournament id: $tournament_id";
 		$plays_table = $this->play_repo->plays_table();
-		$num_rounds = 4;
+		$picks_table = $this->play_repo->picks_table();
+		$results_table = $this->tournament_repo->results_table();
+		$numRounds = 3;
 
-		$sql = "UPDATE $plays_table SET total_score = 10 WHERE bracket_tournament_id = $tournament_id";
+		$num_correct_arr = [];
+		for ($i = 0; $i < $numRounds; $i++) {
+			$num_correct_arr[] = "COALESCE(SUM(CASE WHEN p1.round_index = $i THEN 1 ELSE 0 END), 0) AS round{$i}correct";
+		}
+		$num_correct_select = implode(', ', $num_correct_arr);
+
+		$total_score_arr = [];
+		for ($i = 0; $i < $numRounds; $i++) {
+			$total_score_arr[] = "agg.round{$i}correct * {$point_values[$i]}";
+		}
+		$total_score_exp = implode(' + ', $total_score_arr);
+
+		$sql = "
+		UPDATE $plays_table p0
+		LEFT JOIN (
+				SELECT p1.bracket_play_id,
+								$num_correct_select
+				FROM $picks_table p1
+				JOIN $results_table p2 ON p1.round_index = p2.round_index
+																										AND p1.match_index = p2.match_index
+																										AND p1.winning_team_id = p2.winning_team_id
+																										AND p2.bracket_tournament_id = 3
+				GROUP BY p1.bracket_play_id
+		) agg ON p0.id = agg.bracket_play_id
+		SET p0.total_score = COALESCE($total_score_exp, 0);
+		";
+
+		echo $sql;
+
+		// $sql = "
+		// UPDATE $plays_table p0
+		// LEFT JOIN (
+		// 		SELECT p1.bracket_play_id,
+		// 					COALESCE(SUM(CASE WHEN p1.round_index = 0 THEN 1 ELSE 0 END), 0) AS round1correct,
+		// 					COALESCE(SUM(CASE WHEN p1.round_index = 1 THEN 1 ELSE 0 END), 0) AS round2correct,
+		// 					COALESCE(SUM(CASE WHEN p1.round_index = 2 THEN 1 ELSE 0 END), 0) AS round3correct
+		// 		FROM $picks_table p1
+		// 		JOIN $results_table p2 ON p1.round_index = p2.round_index
+		// 																								AND p1.match_index = p2.match_index
+		// 																								AND p1.winning_team_id = p2.winning_team_id
+		// 																								AND p2.bracket_tournament_id = 3
+		// 		GROUP BY p1.bracket_play_id
+		// ) agg ON p0.id = agg.bracket_play_id
+		// SET p0.total_score = COALESCE(agg.round1correct + agg.round2correct + agg.round3correct, 0);
+		// ";
+
 		$results = $this->wpdb->get_results($sql, ARRAY_A);
 		print_r($results);
 	}
