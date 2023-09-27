01bdc0dd5d66264bf33ccec12edd8244321e260e
Mark <markmaceachen@live.com>
2023-09-27 15:30:48 -0600

    test database table setup

---
+++ tests/phpunit/data/tournaments.php
@@ -0,0 +1,11 @@
+<?php
+
+$tournament_14 = array(
+    "title"=>"Official tournament",
+    "author"=>1,
+    "status"=>"publish",
+    "bracket_template_id"=>4,
+    "tag"=> "bmb_official_tourney",
+);
+
+?>
--- tests/phpunit/tests/test-wp-bracket-builder.php
+++ tests/phpunit/tests/test-wp-bracket-builder.php
@@ -1,4 +1,4 @@
 <?php
 require_once 'test-class-wp-bracket-builder-bracket-play-api.php';
-
+require_once 'test-class-wp-bracket-builder-score-service.php';
 ?>
