f4b79ed90b6961159bcaed5ac15d0bc6ac91d691
Mark <markmaceachen@live.com>
2023-10-04 17:07:30 -0600

    official-tournaments

--- plugin/email/templates/play-scored.php
+++ plugin/email/templates/play-scored.php
@@ -172,7 +172,7 @@
 <td class="pad" style="padding-bottom:10px;padding-left:20px;padding-right:20px;padding-top:10px;">
 <div style="font-family: sans-serif">
 <div class="" style="font-size: 12px; font-family: Arial, Helvetica, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #171620; line-height: 1.2;">
-<p style="margin: 0; font-size: 14px; mso-line-height-alt: 16.8px;"><a href=<?php echo "\"" . get_site_url() . "/tournaments/\"" ?> style="text-decoration: underline; color: #3c3d90;">Tournaments</a></p>
+<p style="margin: 0; font-size: 14px; mso-line-height-alt: 16.8px;"><a href=<?php echo "\"" . get_site_url() . "/official-tournaments/\"" ?> style="text-decoration: underline; color: #3c3d90;">Official Tournaments</a></p>
 </div>
 </div>
 </td>
