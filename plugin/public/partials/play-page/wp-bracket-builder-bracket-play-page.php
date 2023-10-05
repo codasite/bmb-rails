75b465984f3a2c5639ef0a119113b4c1db22fb8e
Barry Molina <bazzaboy@gmail.com>
2023-10-05 18:47:24 -0500

    bust bracket page

--- plugin/public/partials/play-page/wp-bracket-builder-bracket-play-page.php
+++ plugin/public/partials/play-page/wp-bracket-builder-bracket-play-page.php
@@ -4,6 +4,9 @@ switch ($view) {
     case 'view':
         include 'wp-bracket-builder-bracket-view-play.php';
         break;
+    case 'bust':
+        include 'wp-bracket-builder-bracket-bust-play.php';
+        break;
     default:
         include 'wp-bracket-builder-bracket-view-play.php';
         break;
