c5a9eb0e28415f465a6acb171a5aef969ea7d4bf
Barry Molina <bazzaboy@gmail.com>
2023-10-06 21:10:20 -0500

    play history title

--- plugin/public/partials/dashboard/wp-bracket-builder-my-play-history.php
+++ plugin/public/partials/dashboard/wp-bracket-builder-my-play-history.php
@@ -29,7 +29,10 @@ $plays = $play_repo->get_all(
 );
 
 function play_list_item(Wp_Bracket_Builder_Bracket_Play $play) {
-	$tournament_name = $play->tournament->title;
+	$title = $play->title;
+	if (empty($title)) {
+		$title = $play->tournament->title;
+	}
 	$user_rank = 99999;
 	$complete = $play->tournament->status === 'complete';
 	$play_id = $play->id;
@@ -45,7 +48,7 @@ function play_list_item(Wp_Bracket_Builder_Bracket_Play $play) {
 ?>
 	<div class="tw-flex tw-justify-between tw-p-30 tw-rounded-16 tw-border-2 tw-border-solid tw-border-blue/20 tw-bg-blue/5">
 		<div class="tw-flex tw-flex-col tw-gap-20">
-			<h2 class="tw-font-700 tw-text-30 tw-text-white"><?php echo esc_html($tournament_name) ?></h2>
+			<h2 class="tw-font-700 tw-text-30 tw-text-white"><?php echo esc_html($title) ?></h2>
 			<div class="tw-flex tw-gap-16">
 				<!-- View this play and add to apparel -->
 				<?php echo view_play_btn($view_link); ?>
