e2d588da4e19ab14d9898d8d8e8861e8289ee943
Karl Molina <karlgmolina@gmail.com>
2023-10-17 13:20:51 -0600

    Remove unused imports

--- plugin/includes/react-bracket-builder/src/brackets/shared/components/Bracket/PickableBracket.tsx
+++ plugin/includes/react-bracket-builder/src/brackets/shared/components/Bracket/PickableBracket.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MatchNode, Team } from '../../models/MatchTree'
 import { BracketProps } from '../types'
-import { DefaultBracket } from '../Bracket/DefaultBracket'
+import { DefaultBracket } from './DefaultBracket'
 import { TeamSlotToggle } from '../TeamSlot'
 import { Nullable } from '../../../../utils/types'
 
