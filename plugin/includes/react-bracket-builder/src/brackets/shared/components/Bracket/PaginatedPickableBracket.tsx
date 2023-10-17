b0608b7c23bcab780f6491de54ce39965309cf32
Karl Molina <karlgmolina@gmail.com>
2023-10-17 13:17:26 -0600

    Always use PickableBracket

--- plugin/includes/react-bracket-builder/src/brackets/shared/components/Bracket/PaginatedPickableBracket.tsx
+++ plugin/includes/react-bracket-builder/src/brackets/shared/components/Bracket/PaginatedPickableBracket.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { MatchNode, Team } from '../../models/MatchTree'
-import { BracketProps, PaginatedBracketProps } from '../types'
-import { DefaultBracket } from '../Bracket/DefaultBracket'
-import { TeamSlotToggle } from '../TeamSlot'
-import { Nullable } from '../../../../utils/types'
+import { PaginatedBracketProps } from '../types'
 import { PickableBracket } from './PickableBracket'
 import { PaginatedDefaultBracket } from './PaginatedDefaultBracket'
 
 export const PaginatedPickableBracket = (props: PaginatedBracketProps) => {
-  const { matchTree, setMatchTree, BracketComponent = PickableBracket } = props
+  const { matchTree, setMatchTree } = props
 
   console.log('PaginatedPickableBracket', matchTree)
   return (
-    <BracketComponent BracketComponent={PaginatedDefaultBracket} {...props} />
+    <PickableBracket BracketComponent={PaginatedDefaultBracket} {...props} />
   )
 }
