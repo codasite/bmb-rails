61b78fe4a3702e4e6cb2a786239570a8be9a6d7f
Barry Molina <bazzaboy@gmail.com>
2023-10-24 18:43:51 -0500

    trying to stub wc

--- plugin/tests/includes/service/product-integrations/gelato/test-gelato-integration-public-hooks.php
+++ plugin/tests/includes/service/product-integrations/gelato/test-gelato-integration-public-hooks.php
@@ -10,6 +10,14 @@ require_once WPBB_PLUGIN_DIR .
 class GelatoIntgrationPublicHooksTest extends WPBB_UnitTestCase {
   public function test_handle_payment_complete() {
     $wc_mock = $this->createMock(Wpbb_WcFunctions::class);
+    $wc_order_item_stub = $this->createStub('WC_Order_Item');
+    $wc_order_stub = $this->createStub('WC_Order');
+    $wc_order_stub->method('get_items')->willReturn([
+      [
+        'product_id' => 1,
+        'quantity' => 1,
+      ],
+    ]);
     $integration_mock = $this->createMock(Wpbb_GelatoProductIntegration::class);
     $hooks = new Wpbb_GelatoPublicHooks($integration_mock, [
       'wc' => $wc_mock,
