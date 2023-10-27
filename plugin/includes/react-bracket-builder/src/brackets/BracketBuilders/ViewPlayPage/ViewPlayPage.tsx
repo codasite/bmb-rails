65b61eaa9df38bc9fd87167bbbcab67bd62c4d03
Barry Molina <bazzaboy@gmail.com>
2023-10-27 16:45:31 -0500

    add to apparel set cookie from view play

--- plugin/includes/react-bracket-builder/src/brackets/BracketBuilders/ViewPlayPage/ViewPlayPage.tsx
+++ plugin/includes/react-bracket-builder/src/brackets/BracketBuilders/ViewPlayPage/ViewPlayPage.tsx
@@ -60,6 +60,12 @@ const ViewPlayPage = (props: ViewPlayPageProps) => {
   const handleAddToApparel = () => {
     console.log('handleAddToApparel')
     console.log('apparelUrl', apparelUrl)
+    const playId = play?.id
+    if (playId) {
+      //set play id in cookie
+      const expiryDate = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) // 30 days from now
+      document.cookie = `play_id=${playId}; path=/; expires=${expiryDate.toUTCString()}`
+    }
     window.location.href = props.apparelUrl
   }
 
