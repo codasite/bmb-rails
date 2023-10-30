a017e09055b3d511426f749711ee2e9b61751af5
Mark <markmaceachen@live.com>
2023-10-30 13:55:38 -0600

    set cookie called inside bracket_repo->create_item

--- plugin/includes/controllers/class-wpbb-bracket-api.php
+++ plugin/includes/controllers/class-wpbb-bracket-api.php
@@ -199,6 +199,12 @@ class Wpbb_BracketApi extends WP_REST_Controller {
     }
 
     $saved = $this->bracket_repo->add($bracket);
+    // check if user logged in
+    if (!is_user_logged_in()) {
+    // if (get_current_user_id() === 0) 
+      $this->utils->set_cookie('bracket_id', $saved->id);
+    }
+    // chec
     return new WP_REST_Response($saved, 201);
   }
 
