1687f98decc58d90dca81dfe1f22d78569c65d90
Barry Molina <bazzaboy@gmail.com>
2023-11-02 13:18:40 -0500

    paginated results builder folder

---
+++ plugin/includes/react-bracket-builder/src/brackets/BracketBuilders/BracketResultsBuilder/PaginatedResultsBuilder/PickableBracketPage.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { PaginatedPickableBracket } from '../../shared/components/Bracket'
+import darkBracketBg from '../../shared/assets/bracket-bg-dark.png'
+import { MatchTree } from '../../shared/models/MatchTree'
+
+interface PickableBracketPageProps {
+  matchTree?: MatchTree
+  setMatchTree?: (matchTree: MatchTree) => void
+  onFinished?: () => void
+}
+
+export const PickableBracketPage = (props: PickableBracketPageProps) => {
+  const { matchTree, setMatchTree, onFinished } = props
+
+  let containerProps = {
+    className: 'wpbb-reset tw-uppercase tw-dark tw-bg-dd-blue',
+  }
+
+  if (matchTree?.allPicked()) {
+    containerProps['style'] = {
+      backgroundImage: `url(${darkBracketBg})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }
+  }
+  return (
+    <div {...containerProps}>
+      {matchTree && (
+        <PaginatedPickableBracket
+          matchTree={matchTree}
+          setMatchTree={setMatchTree}
+          onFinished={onFinished}
+        />
+      )}
+    </div>
+  )
+}
