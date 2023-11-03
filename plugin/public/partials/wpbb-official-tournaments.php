2b28bb82cc04487be0ec244fc63e7d8a3c8a101c
Mark <markmaceachen@live.com>
2023-11-03 17:50:43 -0600

    adjust logo size using media query

--- plugin/public/partials/wpbb-official-tournaments.php
+++ plugin/public/partials/wpbb-official-tournaments.php
@@ -67,7 +67,7 @@ function wpbb_tournament_sort_buttons() {
 <div class="tw-bg-dd-blue tw-py-100 tw-px-20">
 	<div class="wpbb-reset wpbb-official-tourneys tw-flex tw-flex-col tw-gap-30 tw-max-w-screen-lg tw-mx-auto ">
 		<div class="tw-flex tw-flex-col tw-py-30 tw-gap-15 tw-items-center ">
-			<?php echo file_get_contents(WPBB_PLUGIN_DIR . 'public/assets/icons/logo_dark.svg'); ?>
+			<div class="logo-svg"></div>
 			<h1 class="tw-text-32 sm:tw-text-80 tw-font-700 tw-text-center">Official Brackets</h1>
 		</div>
 		<div class="tw-flex tw-flex-col tw-gap-15">
