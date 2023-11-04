a850193adfd68caac556c5f75466bf9469505388
Barry Molina <bazzaboy@gmail.com>
2023-11-03 20:45:18 -0500

    spacing

--- plugin/public/partials/wpbb-official-tournaments.php
+++ plugin/public/partials/wpbb-official-tournaments.php
@@ -64,9 +64,9 @@ function wpbb_tournament_sort_buttons() {
 }
 
 ?>
-<div class="wpbb-faded-bracket-bg tw-py-60 tw-px-20">
+<div class="wpbb-faded-bracket-bg tw-py-20 sm:tw-py-60 tw-px-20">
 	<div class="wpbb-reset wpbb-official-tourneys tw-flex tw-flex-col tw-gap-30 tw-max-w-screen-lg tw-mx-auto ">
-		<div class="tw-flex tw-flex-col tw-py-30 tw-gap-15 tw-items-center ">
+		<div class="tw-flex tw-flex-col tw-py-20 sm:tw-py-30 tw-gap-15 tw-items-center ">
 			<div class="logo-svg"></div>
 			<h1 class="tw-text-32 sm:tw-text-48 md:tw-text-64 lg:tw-text-80 tw-font-700 tw-text-center">Official Brackets</h1>
 		</div>
