9b0a1d0a6adc1f24bd98977c4ba25fd8253fe674
Barry Molina <bazzaboy@gmail.com>
2023-11-06 08:45:19 -0600

    bust play page show bustee tree

--- plugin/includes/react-bracket-builder/src/brackets/BracketBuilders/BustPlayPage/BustPlayPage.tsx
+++ plugin/includes/react-bracket-builder/src/brackets/BracketBuilders/BustPlayPage/BustPlayPage.tsx
@@ -21,13 +21,12 @@ import { ReactComponent as LightningIcon } from '../../shared/assets/lightning.s
 import { ReactComponent as PlayIcon } from '../../shared/assets/play.svg'
 import { getBracketMeta } from '../../shared/components/Bracket/utils'
 import { WithMatchTree3 } from '../../shared/components/HigherOrder/WithMatchTree'
+import { getBustTrees } from './utils'
 // import redBracketBg from '../../shared/assets/bracket-bg-red.png'
 
 interface BustPlayPageProps {
   bracketMeta: BracketMeta
   setBracketMeta: (bracketMeta: BracketMeta) => void
-  matchTree: MatchTree
-  setMatchTree: (matchTree: MatchTree) => void
   bracketPlay: PlayRes
   redirectUrl: string
   darkMode: boolean
@@ -42,14 +41,13 @@ const BustPlayPage = (props: BustPlayPageProps) => {
     setBracketMeta,
     darkMode,
     setDarkMode,
-    matchTree,
-    setMatchTree,
     bracketPlay: play,
     redirectUrl,
     thumbnailUrl,
   } = props
 
   const [page, setPage] = useState('view')
+  const { busteeTree, setBusteeTree } = getBustTrees()
 
   const handleBustPlay = async () => {
     setPage('bust')
@@ -75,12 +73,12 @@ const BustPlayPage = (props: BustPlayPageProps) => {
       const tree = MatchTree.fromPicks(numTeams, matches, picks)
 
       if (tree) {
-        setMatchTree(tree)
+        setBusteeTree(tree)
       }
     }
   }, [play])
 
-  if (page === 'bust' && matchTree) {
+  if (page === 'bust' && busteeTree) {
     return (
       <BustPlayBuilder
         redirectUrl={redirectUrl}
@@ -101,7 +99,7 @@ const BustPlayPage = (props: BustPlayPageProps) => {
       <div
         className={`tw-flex tw-flex-col tw-items-center tw-max-w-screen-lg tw-m-auto`}
       >
-        {matchTree && (
+        {busteeTree && (
           <>
             <div className="tw-mb-40 tw-mt-40 tw-flex tw-flex-col tw-justify-center tw-items-center">
               <ProfilePicture
@@ -111,7 +109,7 @@ const BustPlayPage = (props: BustPlayPageProps) => {
                 shadow={false}
               />
             </div>
-            <PickableBracket matchTree={matchTree} />
+            <PickableBracket matchTree={busteeTree} />
             <div className="tw-h-[260px] tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-16">
               <ActionButton
                 variant="big-green"
