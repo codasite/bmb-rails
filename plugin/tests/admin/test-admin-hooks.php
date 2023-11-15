d78d784d7631353fbef277fa73f7a45a1800f1d0
Karl Molina <karlgmolina@gmail.com>
2023-11-15 16:35:27 -0700

    Add profile image url metadata

--- plugin/tests/admin/test-admin-hooks.php
+++ plugin/tests/admin/test-admin-hooks.php
@@ -30,7 +30,10 @@ class AdminHooksTest extends WPBB_UnitTestCase {
     ]);
     $this->assertEquals(1, count($posts));
     $this->assertEquals($user->ID, $posts[0]->post_author);
+    $meta = get_post_meta($posts[0]->ID);
+    $this->assertEquals(['wpbb_profile_image_url' => ['']], $meta);
   }
+
   public function test_add_user_with_bmb_vip_role_should_add_new_user_profile_post() {
     $user = self::factory()->user->create_and_get(['role' => 'bmb_vip']);
     $posts = get_posts([
