a446a3582e491fb34aa8a65e5044b493f5e4cbb0
Karl Molina <karlgmolina@gmail.com>
2023-11-16 14:46:16 -0700

    Add play cards to user profile

--- plugin/public/partials/user-profile/wpbb-user-profile.php
+++ plugin/public/partials/user-profile/wpbb-user-profile.php
@@ -7,9 +7,16 @@ require_once WPBB_PLUGIN_DIR . 'includes/domain/class-wpbb-bracket.php';
 require_once WPBB_PLUGIN_DIR . 'public/partials/shared/wpbb-brackets-common.php';
 require_once WPBB_PLUGIN_DIR . 'public/partials/shared/wpbb-pagination-widget.php';
 require_once WPBB_PLUGIN_DIR . 'public/partials/shared/wpbb-partials-constants.php';
+require_once WPBB_PLUGIN_DIR . 'public/partials/wpbb-bracket-cards.php';
 
 $user_profile_repo = new Wpbb_UserProfileRepo();
 $user_profile = $user_profile_repo->get_by_post();
+$play_repo = new Wpbb_BracketPlayRepo();
+$plays = $play_repo->get_all([
+	'posts_per_page' => 6,
+	'tag_slug__in' => ['bmb_vip_play'],
+  'author' => $user_profile->author
+]);
 ?>
 <div class="wpbb-reset wpbb-faded-bracket-bg tw-pt-60 tw-pb-[150px] tw-px-20">
 	<div class="wpbb-reset tw-max-w-screen-lg tw-mx-auto tw-flex-col tw-flex">
@@ -19,15 +26,19 @@ $user_profile = $user_profile_repo->get_by_post();
         <img src="<?php echo $user_profile->thumbnail_url; ?>" alt="PROFILE PICTURE" class="tw-object-cover tw-w-full tw-h-full tw-leading-8">
       </div>
       <div class="tw-flex tw-flex-col tw-items-center md:tw-items-start">
-        <div class="tw-shrink">
+        <div class="tw-shrink tw-mb-20 md:tw-mb-0">
           <span class="tw-text-20 md:tw-text-24 tw-bg-red tw-font-700 tw-rounded-8 tw-px-16 tw-py-4">VIP</span>
         </div>
         <h1 class="tw-text-48 md:tw-text-64 tw-font-700 tw-text-center"><?php echo $user_profile->author_display_name; ?></h1>
       </div>
 		</div>
     <h1 class="tw-text-32 md:tw-text-48 tw-font-700 tw-py-30">Recent Play History</h1>
-
-    <h1 class="tw-text-32 md:tw-text-48 tw-font-700">Brackets</h1>
+    <div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-10">
+      <?php foreach ($plays as $play) : ?>
+        <?php echo wpbb_vip_play_card($play); ?>
+      <?php endforeach; ?>
+    </div>
+    <h1 class="tw-text-32 md:tw-text-48 tw-font-700 tw-pt-60">Brackets</h1>
     <div class="tw-flex tw-justify-start tw-gap-10 tw-py-24 tw-flex-wrap">
       <?php echo wpbb_bracket_sort_buttons(); ?>
     </div>
