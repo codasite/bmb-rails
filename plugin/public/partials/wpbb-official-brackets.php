8e2cb2eab38dd2ecd4d652d365d530d8c38c2a6e
Barry Molina <bazzaboy@gmail.com>
2023-11-17 15:02:49 -0600

    rename official brackets

--- plugin/public/partials/wpbb-official-brackets.php
+++ plugin/public/partials/wpbb-official-brackets.php
@@ -6,7 +6,7 @@ require_once('shared/wpbb-brackets-common.php');
 	<div class="wpbb-reset wpbb-official-brackets tw-flex tw-flex-col tw-gap-30 tw-max-w-screen-lg tw-mx-auto ">
 		<div class="tw-flex tw-flex-col tw-py-20 sm:tw-py-30 tw-gap-15 tw-items-center ">
 			<div class="logo-svg"></div>
-			<h1 class="tw-text-32 sm:tw-text-48 md:tw-text-64 lg:tw-text-80 tw-font-700 tw-text-center">Official Brackets</h1>
+			<h1 class="tw-text-32 sm:tw-text-48 md:tw-text-64 lg:tw-text-80 tw-font-700 tw-text-center">Official BMB Brackets</h1>
 		</div>
 		<div class="tw-flex tw-flex-col tw-gap-15">
       <div class="tw-flex tw-justify-center tw-gap-10 tw-py-11 tw-flex-wrap">
