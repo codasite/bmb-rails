e68a3a6d69d2c5977c731b46ba019b2c44d70de5
Barry Molina <bazzaboy@gmail.com>
2023-11-24 20:53:09 -0600

    chat page

--- plugin/public/partials/bracket-page/wpbb-bracket-chat.php
+++ plugin/public/partials/bracket-page/wpbb-bracket-chat.php
@@ -1,7 +1,19 @@
 <?php
+require_once WPBB_PLUGIN_DIR . 'public/partials/shared/wpbb-brackets-common.php';
+	$post = get_post();
+	$title = $post->post_title;
+	$thumbnail = get_the_post_thumbnail_url($post->ID, 'full');
 ?>
-
-<h1>Hello from chat page!!!</h1>
-<h1>Hello from chat page!!!</h1>
-<!-- <?php wp_list_comments(); ?> -->
-<?php comments_template(); ?>
+<div class="wpbb-reset tw-flex tw-flex-col">
+	<div class="tw-bg-dark-blue tw-flex tw-flex-col tw-items-center tw-gap-30 tw-px-20 tw-pt-60 tw-pb-60">
+		<div class="tw-h-100 tw-w-100 tw-rounded-full tw-bg-white tw-bg-[url(<?php echo $thumbnail ?>)] tw-bg-cover tw-bg-center tw-bg-no-repeat"></div>
+		<div class="tw-flex tw-flex-col tw-items-center tw-gap-10">
+			<h1 class="tw-text-24 tw-text-center"><?php echo $title?></h1>
+			<h3 class="tw-leading-none tw-text-14 tw-font-600 tw-px-10 tw-py-[6px] tw-rounded-16 tw-bg-blue">Chatter</h3>
+		</div>
+	</div>
+	<div class="tw-bg-[#02041d] tw-px-20 tw-py-60">
+		<h2 class="tw-text-48 lg:tw-text-64 tw-text-center">Who You Got?</h2>
+		<?php comments_template(); ?>
+	</div>
+</div>
