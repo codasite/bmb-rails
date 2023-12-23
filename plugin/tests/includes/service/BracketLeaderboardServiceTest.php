68bf30073a74a148b24533263d8f9404fb03ee24
Barry Molina <bazzaboy@gmail.com>
2023-12-23 14:55:46 -0600

    more leaderboard service tests

--- plugin/tests/includes/service/BracketLeaderboardServiceTest.php
+++ plugin/tests/includes/service/BracketLeaderboardServiceTest.php
@@ -2,7 +2,7 @@
 
 use WStrategies\BMB\Includes\Service\BracketLeaderboardService;
 
-class LeaderboardServiceTest extends WPBB_UnitTestCase {
+class BracketLeaderboardServiceTest extends WPBB_UnitTestCase {
   public function test_get_bracket() {
     $bracket = $this->create_bracket();
     $leaderboard = new BracketLeaderboardService($bracket->id);
@@ -39,6 +39,45 @@ class LeaderboardServiceTest extends WPBB_UnitTestCase {
     ]);
 
     $leaderboard = new BracketLeaderboardService($bracket->id);
+    $plays = $leaderboard->get_plays();
+    $this->assertEquals(1, count($plays));
+    $this->assertEquals($play1->id, $plays[0]->id);
+  }
+  //test sorted by accuracy_score
+  public function test_get_plays_sorted_by_accuracy_score() {
+    $bracket = $this->create_bracket();
+    $play1 = $this->create_play([
+      'bracket_id' => $bracket->id,
+      'is_tournament_entry' => true,
+      'accuracy_score' => 0.5,
+    ]);
+    $play2 = $this->create_play([
+      'bracket_id' => $bracket->id,
+      'is_tournament_entry' => true,
+      'accuracy_score' => 0.75,
+    ]);
+
+    $leaderboard = new BracketLeaderboardService($bracket->id);
+    $plays = $leaderboard->get_plays();
+    $this->assertEquals(2, count($plays));
+    $this->assertEquals($play2->id, $plays[0]->id);
+    $this->assertEquals($play1->id, $plays[1]->id);
+  }
+
+  // test only plays for the bracket are returned
+  public function test_get_plays_only_for_bracket() {
+    $bracket1 = $this->create_bracket();
+    $bracket2 = $this->create_bracket();
+    $play1 = $this->create_play([
+      'bracket_id' => $bracket1->id,
+      'is_tournament_entry' => true,
+    ]);
+    $play2 = $this->create_play([
+      'bracket_id' => $bracket2->id,
+      'is_tournament_entry' => true,
+    ]);
+
+    $leaderboard = new BracketLeaderboardService($bracket1->id);
     $plays = $leaderboard->get_plays();
     $this->assertEquals(1, count($plays));
     $this->assertEquals($play1->id, $plays[0]->id);
