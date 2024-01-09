a45ce31fd706cae02e19fe0b48cb8d99784b8582
Karl Molina <karlgmolina@gmail.com>
2024-01-08 17:02:53 -0700

    Uncomment StripePayments

--- plugin/Includes/Service/PaymentProcessors/StripePayments.php
+++ plugin/Includes/Service/PaymentProcessors/StripePayments.php
@@ -1,18 +1,29 @@
 <?php
 namespace WStrategies\BMB\Includes\Service\PaymentProcessors;
 
+use WStrategies\BMB\Includes\Repository\BracketRepo;
+use WStrategies\BMB\Includes\Service\BracketProduct\BracketProductUtils;
+
 class StripePayments {
-  // public function create_payment_intent_for_paid_bracket($bracket_id) {
-// 	$bracket = $this->bracket_repo->get($bracket_id);
-// 	$fee = $this->bracket_product_utils->get_bracket_fee($bracket_id);
-// 	$amount = $fee * 100;
-// 	$intent = $this->stripe->paymentIntents->create([
-// 		'amount' => $amount,
-// 		'currency' => 'usd',
-// 		'metadata' => [
-// 			'bracket_id' => $bracket_id,
-// 		],
-// 	]);
-// 	return $intent;
-// }
+  private $bracket_repo;
+  private $bracket_product_utils;
+
+  public function __construct() {
+    $this->bracket_repo = new BracketRepo();
+    $this->bracket_product_utils = new BracketProductUtils();
+  }
+
+  public function create_payment_intent_for_paid_bracket($bracket_id) {
+    $bracket = $this->bracket_repo->get($bracket_id);
+    $fee = $this->bracket_product_utils->get_bracket_fee($bracket_id);
+    $amount = $fee * 100;
+    $intent = $this->stripe->paymentIntents->create([
+      'amount' => $amount,
+      'currency' => 'usd',
+      'metadata' => [
+        'bracket_id' => $bracket_id,
+      ],
+    ]);
+    return $intent;
+  }
 }
