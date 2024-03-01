484f66f47cec74835090f43042f5a95473e6a3da
Barry Molina <bazzaboy@gmail.com>
2024-03-01 17:07:46 -0600

    add some BracketResultsNotificationService unit tests

---
+++ plugin/Includes/Repository/Fakes/PlayRepoFake.php
@@ -0,0 +1,9 @@
+<?php
+namespace WStrategies\BMB\Includes\Repository\Fakes;
+
+use WStrategies\BMB\Includes\Repository\PlayRepo;
+
+class PlayRepoFake extends PlayRepo {
+  public function __construct(array $args = []) {
+  }
+}
--- plugin/tests/unit/Includes/Service/Notifications/BracketResultsNotificationServiceTest.php
+++ plugin/tests/unit/Includes/Service/Notifications/BracketResultsNotificationServiceTest.php
@@ -29,12 +29,23 @@ class BracketResultsNotificationServiceTest extends TestCase {
 
   public function test_should_send_email_when_match_pick_result_is_not_null() {
     $bracket = new Bracket(['id' => 1]);
-    $plays = [$this->createMock(BracketPlay::class)];
+    $plays = [new BracketPlay(['picks' => []])];
     $play_repo = $this->createMock(PlayRepo::class);
     $play_repo->method('get_all')->willReturn($plays);
     $email_format_service = $this->createMock(
       BracketResultsEmailFormatService::class
     );
+    $match_pick_result_factory = $this->createMock(
+      MatchPickResultFactory::class
+    );
+    $results_filter_service = $this->createMock(
+      BracketResultsFilterService::class
+    );
+    $results_filter_service
+      ->method('filter_results_updated_at_time')
+      ->willReturn([]);
+    $match_service = $this->createMock(BracketMatchService::class);
+    $match_service->method('matches_from_picks')->willReturn([]);
     $result_to_send = $this->createMock(MatchPickResult::class);
     // Expecting the send_email method to be called once with the result to send
     $email_format_service
@@ -53,10 +64,96 @@ class BracketResultsNotificationServiceTest extends TestCase {
       'email_service' => $this->createMock(EmailServiceInterface::class),
       'play_repo' => $play_repo,
       'results_sent_at_repo' => $this->createMock(DateTimePostMetaRepo::class),
-      'results_filter_service' => $this->createMock(
-        BracketResultsFilterService::class
+      'results_filter_service' => $results_filter_service,
+      'match_service' => $match_service,
+      'match_pick_result_factory' => $this->createMock(
+        MatchPickResultFactory::class
       ),
-      'match_service' => $this->createMock(BracketMatchService::class),
+      'email_format_service' => $email_format_service,
+      'match_pick_result_notification_service' => $match_pick_result_notification_service,
+    ]);
+
+    $notification_service->notify_bracket_results_updated($bracket);
+  }
+  public function test_should_not_send_email_when_match_pick_result_is_null() {
+    $bracket = new Bracket(['id' => 1]);
+    $plays = [new BracketPlay(['picks' => []])];
+    $play_repo = $this->createMock(PlayRepo::class);
+    $play_repo->method('get_all')->willReturn($plays);
+    $email_format_service = $this->createMock(
+      BracketResultsEmailFormatService::class
+    );
+    $match_pick_result_factory = $this->createMock(
+      MatchPickResultFactory::class
+    );
+    $results_filter_service = $this->createMock(
+      BracketResultsFilterService::class
+    );
+    $results_filter_service
+      ->method('filter_results_updated_at_time')
+      ->willReturn([]);
+    $match_service = $this->createMock(BracketMatchService::class);
+    $match_service->method('matches_from_picks')->willReturn([]);
+    $match_pick_result_notification_service = $this->createMock(
+      MatchPickResultNotificationService::class
+    );
+    $match_pick_result_notification_service
+      ->method('get_match_pick_result_for_play')
+      ->willReturn(null);
+
+    $notification_service = new BracketResultsNotificationService([
+      'email_service' => $this->createMock(EmailServiceInterface::class),
+      'play_repo' => $play_repo,
+      'results_sent_at_repo' => $this->createMock(DateTimePostMetaRepo::class),
+      'results_filter_service' => $results_filter_service,
+      'match_service' => $match_service,
+      'match_pick_result_factory' => $this->createMock(
+        MatchPickResultFactory::class
+      ),
+      'email_format_service' => $email_format_service,
+      'match_pick_result_notification_service' => $match_pick_result_notification_service,
+    ]);
+
+    $email_format_service->expects($this->never())->method('send_email');
+
+    $notification_service->notify_bracket_results_updated($bracket);
+  }
+
+  public function test_should_set_results_sent_at_to_now_after_sending_notifications() {
+    $bracket = new Bracket(['id' => 1]);
+    $plays = [new BracketPlay(['picks' => []])];
+    $play_repo = $this->createMock(PlayRepo::class);
+    $play_repo->method('get_all')->willReturn($plays);
+    $email_format_service = $this->createMock(
+      BracketResultsEmailFormatService::class
+    );
+    $match_pick_result_factory = $this->createMock(
+      MatchPickResultFactory::class
+    );
+    $results_filter_service = $this->createMock(
+      BracketResultsFilterService::class
+    );
+    $results_filter_service
+      ->method('filter_results_updated_at_time')
+      ->willReturn([]);
+    $match_service = $this->createMock(BracketMatchService::class);
+    $match_service->method('matches_from_picks')->willReturn([]);
+    $match_pick_result_notification_service = $this->createMock(
+      MatchPickResultNotificationService::class
+    );
+    $match_pick_result_notification_service
+      ->method('get_match_pick_result_for_play')
+      ->willReturn(null);
+
+    $results_sent_at_repo = $this->createMock(DateTimePostMetaRepo::class);
+    $results_sent_at_repo->expects($this->once())->method('set_to_now');
+
+    $notification_service = new BracketResultsNotificationService([
+      'email_service' => $this->createMock(EmailServiceInterface::class),
+      'play_repo' => $play_repo,
+      'results_sent_at_repo' => $results_sent_at_repo,
+      'results_filter_service' => $results_filter_service,
+      'match_service' => $match_service,
       'match_pick_result_factory' => $this->createMock(
         MatchPickResultFactory::class
       ),
