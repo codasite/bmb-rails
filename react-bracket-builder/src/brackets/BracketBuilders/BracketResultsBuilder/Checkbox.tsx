ae4019d6f9724e7c015f7d982a88d92a423e37bf
Karl Molina <karlgmolina@gmail.com>
2024-03-04 15:31:23 -0700

    Rename CustomCheckbox to Checkbox

--- react-bracket-builder/src/brackets/BracketBuilders/BracketResultsBuilder/Checkbox.tsx
+++ react-bracket-builder/src/brackets/BracketBuilders/BracketResultsBuilder/Checkbox.tsx
@@ -1,26 +1,19 @@
 import checkIcon from '../../shared/assets/check.svg'
 
-export interface CustomCheckboxProps {
+export const Checkbox = (props: {
   id: string
   checked: boolean
   onChange: (e: any) => void
   height?: number
   width?: number
-}
-
-export const CustomCheckbox = (props: any) => {
+}) => {
   const { id, checked, onChange, height = 32, width = 32 } = props
-
   const baseStyles = ['tw-appearance-none', 'tw-rounded-8', 'tw-cursor-pointer']
-
   const uncheckedStyles = ['tw-border', 'tw-border-solid', 'tw-border-white']
-
   const checkedStyles = ['tw-bg-white', 'tw-bg-no-repeat', 'tw-bg-center']
-
   const styles = baseStyles
     .concat(checked ? checkedStyles : uncheckedStyles)
     .join(' ')
-
   return (
     <input
       type="checkbox"
