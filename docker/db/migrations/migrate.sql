5efdb08720b969698f53c7fb96888cb679a301e7
Karl Molina <karlgmolina@gmail.com>
2024-08-20 16:34:28 -0600

    Update migrate.sql

--- docker/db/migrations/migrate.sql
+++ docker/db/migrations/migrate.sql
@@ -5,3 +5,7 @@ ALTER TABLE wp_bracket_builder_plays ADD COLUMN is_tournament_entry TINYINT(1) N
 ALTER TABLE wp_bracket_builder_plays ADD COLUMN is_paid TINYINT(1) NOT NULL DEFAULT 0;
 
 ALTER TABle wp_bracket_builder_bracket_results ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+ALTER TABLE wp_bracket_builder_brackets
+        ADD COLUMN isVoting TINYINT(1) DEFAULT 0,
+        ADD COLUMN liveRoundIndex TINYINT(2) DEFAULT 0;
