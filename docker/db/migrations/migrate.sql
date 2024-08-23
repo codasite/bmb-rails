302f710b651d2fa7cd203772b93b9a12f05a84ac
Barry Molina <bazzaboy@gmail.com>
2024-08-23 15:02:43 -0500

    update migrations

--- docker/db/migrations/migrate.sql
+++ docker/db/migrations/migrate.sql
@@ -4,8 +4,10 @@ ALTER TABLE wp_bracket_builder_plays ADD COLUMN bmb_official TINYINT(1) NOT NULL
 ALTER TABLE wp_bracket_builder_plays ADD COLUMN is_tournament_entry TINYINT(1) NOT NULL DEFAULT 0;
 ALTER TABLE wp_bracket_builder_plays ADD COLUMN is_paid TINYINT(1) NOT NULL DEFAULT 0;
 
-ALTER TABle wp_bracket_builder_bracket_results ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABlE wp_bracket_builder_bracket_results ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
 ALTER TABLE wp_bracket_builder_brackets
         ADD COLUMN isVoting TINYINT(1) DEFAULT 0,
         ADD COLUMN liveRoundIndex TINYINT(2) DEFAULT 0;
+
+ALTER TABlE wp_bracket_builder_bracket_results ADD COLUMN winning_team_pick_percent DECIMAL(6, 5) DEFAULT NULL;
