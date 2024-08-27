35dc477b59532d6fc13ab6f3389e1e305fae909d
Barry Molina <bazzaboy@gmail.com>
2024-08-27 08:12:56 -0500

    fix naming in migrate.sql

--- docker/db/migrations/migrate.sql
+++ docker/db/migrations/migrate.sql
@@ -7,7 +7,7 @@ ALTER TABLE wp_bracket_builder_plays ADD COLUMN is_paid TINYINT(1) NOT NULL DEFA
 ALTER TABlE wp_bracket_builder_bracket_results ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
 ALTER TABLE wp_bracket_builder_brackets
-        ADD COLUMN isVoting TINYINT(1) DEFAULT 0,
-        ADD COLUMN liveRoundIndex TINYINT(2) DEFAULT 0;
+        ADD COLUMN is_voting TINYINT(1) DEFAULT 0,
+        ADD COLUMN live_round_index TINYINT(2) DEFAULT 0;
 
 ALTER TABlE wp_bracket_builder_bracket_results ADD COLUMN popularity DECIMAL(6, 5) DEFAULT NULL;
