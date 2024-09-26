0b1e116f999cb2af6f3370981375e7a6c8db3768
Barry Molina <bazzaboy@gmail.com>
2024-09-26 18:09:55 -0500

    sign in/register button styling

--- react-bracket-builder/src/brackets/BracketBuilders/PlayBracketBuilder/SubmitPicksRegisterModal.tsx
+++ react-bracket-builder/src/brackets/BracketBuilders/PlayBracketBuilder/SubmitPicksRegisterModal.tsx
@@ -13,10 +13,10 @@ export default function SubmitPicksRegisterModal(props: {
     <Modal show={show} setShow={setShow}>
       <ModalHeader text={'Sign in or register to submit your picks!'} />
       <div className="tw-flex tw-flex-col tw-gap-10">
-        <Link href={props.signInUrl}>
+        <Link href={props.signInUrl} color="green">
           <span>Sign in</span>
         </Link>
-        <Link href={props.registerUrl}>
+        <Link href={props.registerUrl} variant="filled">
           <span>Register</span>
         </Link>
       </div>
