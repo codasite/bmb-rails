57a8c3f1a9d1898dfee1ba9ed574bce8a0473bef
Barry Molina <bazzaboy@gmail.com>
2025-01-10 06:38:05 -0600

    update BracketResultsNotificationServiceTest to use new structure

--- plugin/tests/unit/Includes/Service/Notifications/BracketResultsNotificationServiceTest.php
+++ plugin/tests/unit/Includes/Service/Notifications/BracketResultsNotificationServiceTest.php
@@ -11,36 +11,33 @@ use WStrategies\BMB\Includes\Repository\BracketRepo;
 use WStrategies\BMB\Includes\Repository\DateTimePostMetaRepo;
 use WStrategies\BMB\Includes\Repository\PlayRepo;
 use WStrategies\BMB\Includes\Service\BracketMatchService;
-use WStrategies\BMB\Features\Bracket\BracketResults\BracketResultsEmailFormatService;
+use WStrategies\BMB\Features\Bracket\BracketResults\Fakes\BracketResultsNotificationListenerFake;
 use WStrategies\BMB\Features\Bracket\BracketResults\BracketResultsFilterService;
 use WStrategies\BMB\Features\Bracket\BracketResults\BracketResultsNotificationService;
 use WStrategies\BMB\Features\Notifications\Email\EmailServiceInterface;
 use WStrategies\BMB\Includes\Service\PickResultService;
+use WStrategies\BMB\Includes\Repository\Fakes\UserRepoFake;
 
 class BracketResultsNotificationServiceTest extends TestCase {
   public function test_should_send_email_when_match_pick_result_is_not_null() {
     $bracket = new Bracket(['id' => 1]);
     $bracket_repo = $this->createStub(BracketRepo::class);
-    $plays = [new Play(['picks' => []])];
+    $plays = [new Play(['picks' => [], 'author' => 1])];
     $play_repo = $this->createMock(PlayRepo::class);
     $play_repo->method('get_all')->willReturn($plays);
-    $email_format_service = $this->createMock(
-      BracketResultsEmailFormatService::class
-    );
+
+    $notification_listener = new BracketResultsNotificationListenerFake();
+
     $results_filter_service = $this->createMock(
       BracketResultsFilterService::class
     );
     $results_filter_service
       ->method('filter_results_updated_at_time')
       ->willReturn([$this->createStub(Pick::class)]);
+
     $match_service = $this->createMock(BracketMatchService::class);
     $match_service->method('matches_2d_from_picks')->willReturn([]);
     $result_to_send = $this->createMock(PickResult::class);
-    // Expecting the send_email method to be called once with the result to send
-    $email_format_service
-      ->expects($this->once())
-      ->method('send_email')
-      ->with($plays[0], $result_to_send);
 
     $pick_result_service = $this->createMock(PickResultService::class);
     $pick_result_service
@@ -57,27 +54,31 @@ class BracketResultsNotificationServiceTest extends TestCase {
       'match_pick_result_factory' => $this->createMock(
         PickResultFactory::class
       ),
-      'email_format_service' => $email_format_service,
+      'listeners' => [$notification_listener],
       'pick_result_service' => $pick_result_service,
+      'user_repo' => new UserRepoFake(),
     ]);
 
     $notification_service->send_results_notifications_for_bracket($bracket);
+    $this->assertTrue($notification_listener->notify_was_called);
   }
+
   public function test_should_not_send_email_when_match_pick_result_is_null() {
     $bracket = new Bracket(['id' => 1]);
     $bracket_repo = $this->createStub(BracketRepo::class);
-    $plays = [new Play(['picks' => []])];
+    $plays = [new Play(['picks' => [], 'author' => 1])];
     $play_repo = $this->createMock(PlayRepo::class);
     $play_repo->method('get_all')->willReturn($plays);
-    $email_format_service = $this->createMock(
-      BracketResultsEmailFormatService::class
-    );
+
+    $notification_listener = new BracketResultsNotificationListenerFake();
+
     $results_filter_service = $this->createMock(
       BracketResultsFilterService::class
     );
     $results_filter_service
       ->method('filter_results_updated_at_time')
       ->willReturn([]);
+
     $match_service = $this->createMock(BracketMatchService::class);
     $match_service->method('matches_2d_from_picks')->willReturn([]);
     $pick_result_service = $this->createMock(PickResultService::class);
@@ -93,30 +94,31 @@ class BracketResultsNotificationServiceTest extends TestCase {
       'match_pick_result_factory' => $this->createMock(
         PickResultFactory::class
       ),
-      'email_format_service' => $email_format_service,
+      'listeners' => [$notification_listener],
       'pick_result_service' => $pick_result_service,
+      'user_repo' => new UserRepoFake(),
     ]);
 
-    $email_format_service->expects($this->never())->method('send_email');
-
     $notification_service->send_results_notifications_for_bracket($bracket);
+    $this->assertFalse($notification_listener->notify_was_called);
   }
 
   public function test_should_set_results_sent_at_to_now_after_sending_notifications() {
     $bracket = new Bracket(['id' => 1]);
     $bracket_repo = $this->createStub(BracketRepo::class);
-    $plays = [new Play(['picks' => []])];
+    $plays = [new Play(['picks' => [], 'author' => 1])];
     $play_repo = $this->createMock(PlayRepo::class);
     $play_repo->method('get_all')->willReturn($plays);
-    $email_format_service = $this->createMock(
-      BracketResultsEmailFormatService::class
-    );
+
+    $notification_listener = new BracketResultsNotificationListenerFake();
+
     $results_filter_service = $this->createMock(
       BracketResultsFilterService::class
     );
     $results_filter_service
       ->method('filter_results_updated_at_time')
       ->willReturn([$this->createStub(Pick::class)]);
+
     $match_service = $this->createMock(BracketMatchService::class);
     $match_service->method('matches_2d_from_picks')->willReturn([]);
     $pick_result_service = $this->createMock(PickResultService::class);
@@ -135,8 +137,9 @@ class BracketResultsNotificationServiceTest extends TestCase {
       'match_pick_result_factory' => $this->createMock(
         PickResultFactory::class
       ),
-      'email_format_service' => $email_format_service,
+      'listeners' => [$notification_listener],
       'pick_result_service' => $pick_result_service,
+      'user_repo' => new UserRepoFake(),
     ]);
 
     $notification_service->send_results_notifications_for_bracket($bracket);
