15f7f9b6afdb198f5fa5a253cefe86b2093d827e
Barry Molina <bazzaboy@gmail.com>
2025-02-10 11:58:30 -0600

    bump patch, change base url

--- bmb_mobile/lib/features/wp_http/wp_urls.dart
+++ bmb_mobile/lib/features/wp_http/wp_urls.dart
@@ -1,10 +1,10 @@
 import 'package:bmb_mobile/core/utils/environment_config.dart';
 
 class WpUrls {
-  // static const String baseUrl = EnvironmentConfig.wpBaseUrl;
+  static const String baseUrl = EnvironmentConfig.wpBaseUrl;
   // static const String baseUrl = "https://backmybracket.com";
   // static const String baseUrl = "http://localhost:8008";
-  static const String baseUrl = "http://192.168.1.77:8008";
+  // static const String baseUrl = "http://192.168.1.77:8008";
   // static const String baseUrl = "http://172.20.10.8:8008";
 
   // WordPress API paths
