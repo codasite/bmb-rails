af8510b40869b76b592babdab4fe00a24b2f6730
Barry Molina <bazzaboy@gmail.com>
2025-02-17 09:34:13 -0600

    anyone can duplicate a bracket

--- plugin/Public/Partials/shared/BracketOptionPermissions.php
+++ plugin/Public/Partials/shared/BracketOptionPermissions.php
@@ -14,15 +14,13 @@ class BracketOptionPermissions {
       case 'set_fee':
         return current_user_can('wpbb_add_bracket_fee', $bracket->id);
 
-      case 'duplicate_bracket':
-        return current_user_can('wpbb_edit_bracket', $bracket->id);
-
       case 'lock_tournament':
         return current_user_can('wpbb_edit_bracket', $bracket->id);
 
       // These options don't have explicit permission checks in the original code
       case 'most_popular_picks':
       case 'share_bracket':
+      case 'duplicate_bracket':
         return true;
 
       default:
