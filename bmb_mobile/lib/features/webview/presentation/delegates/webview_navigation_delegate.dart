2a032f43aefd4bed14d59d68b4b7f57a870d5d58
Barry Molina <bazzaboy@gmail.com>
2025-03-06 14:57:16 -0600

    open in browser if url contains has_fee=true

--- bmb_mobile/lib/features/webview/presentation/delegates/webview_navigation_delegate.dart
+++ bmb_mobile/lib/features/webview/presentation/delegates/webview_navigation_delegate.dart
@@ -14,6 +14,7 @@ class WebViewNavigationDelegate extends NavigationDelegate {
     '/shop/bmb/',
     '/referralprogram/',
     '/be-a-host/',
+    'has_fee=true',
   ];
 
   // Domains that are allowed to load in WebView
