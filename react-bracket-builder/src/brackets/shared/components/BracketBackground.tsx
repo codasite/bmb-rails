08a6330d29afa2adf7a3aaadc19236aa8f6895b3
Barry Molina <bazzaboy@gmail.com>
2025-03-16 11:57:57 -0500

    update bracket background to optionally not show image

--- react-bracket-builder/src/brackets/shared/components/BracketBackground.tsx
+++ react-bracket-builder/src/brackets/shared/components/BracketBackground.tsx
@@ -7,22 +7,28 @@ import { useContext } from 'react'
 interface BracketBackgroundProps {
   children: React.ReactNode
   className?: string
+  useImageBackground?: boolean
 }
 
 export const BracketBackground = ({
   children,
   className = '',
+  useImageBackground = true,
 }: BracketBackgroundProps) => {
   const { darkMode } = useContext(DarkModeContext)
   const backgroundUrl = darkMode ? darkBracketBg : lightBracketBg
-  const backgroundStyle = `url(${backgroundUrl})`
+  const backgroundStyle = useImageBackground
+    ? `url(${backgroundUrl})`
+    : darkMode
+    ? '#010433'
+    : 'white'
 
   return (
     <div
       className={`wpbb-reset tw-uppercase tw-bg-no-repeat tw-bg-top tw-bg-cover tw-min-h-screen tw-px-20 ${
         darkMode ? 'tw-dark' : ''
       } ${className}`}
-      style={{ backgroundImage: backgroundStyle }}
+      style={{ background: backgroundStyle }}
     >
       {children}
     </div>
