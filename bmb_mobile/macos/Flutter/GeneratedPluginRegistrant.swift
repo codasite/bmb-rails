64d88ebf628821208360e9be413088fa20e0ce05
Barry Molina <bazzaboy@gmail.com>
2025-06-25 12:23:53 -0500

    update flutter dependencies

--- bmb_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bmb_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -24,5 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
